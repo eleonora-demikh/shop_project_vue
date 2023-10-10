9e654ec4cc2017a3db19080bd20530a69b9eccc0
Eleonora Demikh <deademikh@gmail.com>
2023-10-10 20:38:03 +0300

    v 1.2

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import VitePluginGhPages from 'vite-plugin-gh-pages'
-
-const ghPagesOptions = {
-  branch: 'gh-pages',
-};
+import {VitePluginGhPages} from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    VitePluginGhPages(ghPagesOptions),
+    VitePluginGhPages(),
   ],
   resolve: {
     alias: {
